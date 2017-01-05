98f268c1364ea171ec9b8312dad88f51d5d7cb25
odow <o.dowson@gmail.com>
2017-01-05 22:40:07 +1300

    fix size of x

--- src/cut_definitions.jl
+++ src/cut_definitions.jl
@@ -32,6 +32,7 @@ end
 """
     This function assembles a new cut using the following inputs
     + oracle::CutOracle - used to dispatch
+    + sense::Sense - either ::Maximum or ::Minimum
     + x::Vector{Vector{Float64}} - a vector of vector of state values for each scenario
     + pi::Vector{Vector{Float64}} - a vector of vector of dual values for each scenario
     + theta::Vector{Float64} - a vector of objective values for each scenario
@@ -51,11 +52,11 @@ cutgenerator(oracle::CutOracle, sense::AbstactSense, x, pi, theta, prob) = error
 """)
 
 function cutgenerator(ex::Expectation, sense::AbstactSense, x, pi, theta, prob)
-    @assert length(x) == length(pi) == length(theta) == length(prob)
-    intercept = theta[1] - dot(pi[1], x[1])
+    @assert length(pi) == length(theta) == length(prob)
+    intercept = theta[1] - dot(pi[1], x)
     coefficients = pi[1] * prob[1]
     @inbounds for i=2:length(prob)
-        intercept += theta[i] - dot(pi[i], x[i])
+        intercept += theta[i] - dot(pi[i], x)
         coefficients += pi[i] * prob[i]
     end
     return Cut(intercept, coefficients)
@@ -64,7 +65,7 @@ end
 # λ * E[x] + (1 - λ) * CVaR(β)[x]
 const expectation = Expectation()
 function cutgenerator(cvar::NestedCVaR, sense::AbstractSense, x, pi, theta, prob)
-    @assert length(x) == length(pi) == length(theta) == length(prob)
+    @assert length(pi) == length(theta) == length(prob)
     if length(cvar.storage) < length(prob)
         append!(cvar.storage, zeros(length(prob) - length(cvar.storage)))
     end
@@ -75,12 +76,12 @@ end
 
 _sortperm(::Maximum, x) = sortperm(x, rev=false)
 _sortperm(::Minimum, x) = sortperm(x, rev=true)
-function calculateCVaRprobabilities!(newprob, sense, oldprob, x, lambda, beta::Float64)
+function calculateCVaRprobabilities!(newprob, sense, oldprob, theta, lambda, beta::Float64)
     @assert length(newprob) >= length(oldprob)
     quantile_collected = 0.0
     cvarprob = 0.0
     cache = (1 - lambda) / beta             # cache this to save some operations
-    @inbounds for i in _sortperm(sense, x)        # For each scenario in order
+    @inbounds for i in _sortperm(sense, theta)        # For each scenario in order
         newprob[i] = lambda * oldprob[i]    # expectation contribution
         if quantile_collected <  beta       # We haven't collected the beta quantile
             cvarprob = min(oldprob[i], beta-quantile_collected)
